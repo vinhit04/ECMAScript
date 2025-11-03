abe81aae374bce7114b9312e9d3770efdd095255
Vu Phuc Quang Vinh <vinhvpq2004@gmail.com>
2025-11-03 13:31:56 +0700

    Refactor BMI calculation function by removing commented code for clarity

--- lab1.js
+++ lab1.js
@@ -1,20 +1,20 @@
-// // bai 1 : viet ham chi so BMI va phan loai
-// function tinhBMI(canNang, chieuCao) {
-//     let bmi = canNang / (chieuCao * chieuCao);
-//     let ketluan = '';
+// bai 1 : viet ham chi so BMI va phan loai
+function tinhBMI(canNang, chieuCao) {
+    let bmi = canNang / (chieuCao * chieuCao);
+    let ketluan = '';
 
-//     if (bmi < 18.5) {
-//         ketluan = 'Thiếu cân';
-//     } else if (bmi < 24.9) {
-//         ketluan = 'Cân nặng bình thường';
-//     } else if (bmi < 29.9) {
-//         ketluan = 'Thừa cân';
-//     } else {
-//         ketluan = 'Béo phì';
-//     }
-//     return { bmi, ketluan };
-// }
-// console.log(tinhBMI(92,1.75));
+    if (bmi < 18.5) {
+        ketluan = 'Thiếu cân';
+    } else if (bmi < 24.9) {
+        ketluan = 'Cân nặng bình thường';
+    } else if (bmi < 29.9) {
+        ketluan = 'Thừa cân';
+    } else {
+        ketluan = 'Béo phì';
+    }
+    return { bmi, ketluan };
+}
+console.log(tinhBMI(92,1.75));
 
 
 // bai 2 : Tao doi tuong quan ly sach su dung Enhance Object Literals
